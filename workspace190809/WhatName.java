223739f1485bb60a06706495093116bd9c3e16cf
YEOYOHAN <maybehans@gmail.com>
2019-08-09 18:12:41 +0900

    190809 08:12

--- workspace190809/WhatName.java
+++ workspace190809/WhatName.java
@@ -1,8 +1,15 @@
-/**
- * ???? ??��??? ?????? ???? comment(???)
- */
-class WhatName{// Definition(????)
+import java.util.Scanner;
+class WhatName{
     public static void main(String[] args){
-        System.out.print("What Name"); // ??��?
+        System.out.println("이름 뭐니 ?");
+        String name = "";
+        // 값을 입력받는 지점
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+        System.out.println("이름은 " + name + "이다." );
+        System.out.println("몇살이에요?");
+        int age = 0;
+        age = scanner.nextInt();
+        System.out.print("나이는 "+ age +"살이다.");
     }
-}//????(body)
+}
